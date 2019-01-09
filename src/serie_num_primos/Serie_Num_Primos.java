909b23c9e3ecc23bfb609c135b61c156dfa8435b
jowa <jowa@dell>
2019-01-08 23:17:12 -0500

    Codigo Base donde se genera la serie de numeros Primos, pero solo se presenta por pantalla, para verificar resultados

---
+++ src/serie_num_primos/Serie_Num_Primos.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+
+public class Serie_Num_Primos {
+
+    public static void main(String[] args) throws FileNotFoundException{
+        //PrintStream file = new PrintStream("Resultado.txt");
+        Scanner e = new Scanner(System.in);
+        System.out.print("Serie de Numeros Primos \n");
+        System.out.print("Ingresa el numero de elementos de la serie: ");
+               
+        int numIng = e.nextInt();
+        int c = 1;
+        int p = 2;
+        int d = 2;
+        
+        while (c <= numIng) {
+            if (p % d == 0) {
+                if (p == d) {
+                    System.out.print(p + ", ");
+                    c++;
+                }
+                d = 2;
+                p++;
+            } else {
+                d++;
+            }
+        }
+    }
+}
