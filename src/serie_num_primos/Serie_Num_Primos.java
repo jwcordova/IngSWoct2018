861cb6a3556d9952f55380634fef3ad938d0aad5
jowa <jowa@dell>
2019-01-09 10:16:43 -0500

    En el codigo de la serie se implementa la suma total de la serie generada, la misma es presentada por pantalla y agregada al archivo de resultados

--- src/serie_num_primos/Serie_Num_Primos.java
+++ src/serie_num_primos/Serie_Num_Primos.java
@@ -9,18 +9,18 @@ public class Serie_Num_Primos {
         Scanner e = new Scanner(System.in);
         System.out.print("Serie de Numeros Primos \n");
         System.out.print("Ingresa el numero de elementos de la serie: ");
-               
         int numIng = e.nextInt();
         int c = 1;
         int p = 2;
         int d = 2;
-        
+        int sum = 0;
         while (c <= numIng) {
             if (p % d == 0) {
                 if (p == d) {
-                    //System.out.print(p + ", ");
+                    System.out.print(p + ", ");
                     file.print(p + ", ");
                     c++;
+                    sum=sum+p;
                 }
                 d = 2;
                 p++;
@@ -28,6 +28,9 @@ public class Serie_Num_Primos {
                 d++;
             }
         }
-        System.out.print("Ahora los resultados seran generados en un archivo .txt");
-    }
+        System.out.print("\nLa suma de la Serie de Numeros Primos es:\t" + sum );
+        System.out.print("\nAhora los resultados seran generados en un archivo .txt");
+        
+        file.print("\r\n" + "La suma de la Serie de Numeros Primos es:\t" + sum);
+      }
 }
